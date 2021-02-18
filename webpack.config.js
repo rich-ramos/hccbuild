a4aab48f962f4adc43c17fc42e6659900d758931
Rich Ramos <richard.ramos3124@gmail.com>
2021-02-18 17:01:56 -0500

    added media mixins and applied to large-hero.css

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const postCSSPlugins = {
     plugins: [
         require('postcss-import'),
+        require('postcss-mixins'),
         require('postcss-simple-vars'),
         require('postcss-nested'),
         require('autoprefixer'),
