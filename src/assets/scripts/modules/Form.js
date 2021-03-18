eef1ba64265adf599630ed7fd0b0c1b6e655c8cf
Rich Ramos <richard.ramos3124@gmail.com>
2021-03-17 23:27:16 -0400

    Updated Form.js to handle form submissions. Added _form-message class

--- src/assets/scripts/modules/Form.js
+++ src/assets/scripts/modules/Form.js
@@ -1,6 +1,7 @@
 class Form {
     constructor() {
         this.form = document.querySelector("form");
+        this.formInputs = document.querySelectorAll(".form__input");
         this.events();
     }
 
@@ -10,22 +11,44 @@ class Form {
 
     handleSubmit(e) {
         e.preventDefault();
-        let myForm = document.querySelector(".form");
-        let formData = new FormData(myForm);
-        fetch('/', {
-            method: 'POST',
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: new URLSearchParams(formData).toString()
-        }).then(() => this.displaySuccessfulMessage()).catch((error) => alert(error));
+        var isValid = this.validateFormInputs();
+        if (isValid) {
+            this.displayMessage("Form Submitted", "success");
+            this.clearFormInputs();
+        } else {
+            this.displayMessage("Form not submitted", "failure");
+        }
     }
 
-    displaySuccessfulMessage() {
-        this.form.insertAdjacentElement("beforebegin", 
+    clearFormInputs() {
+        this.formInputs.forEach((el) => el.value = '');
+    }
+
+    validateFormInputs() {
+        var isValid = true;
+        for (var element of this.formInputs.values()) {
+            if (element.value === '') {
+                isValid = false;
+            }
+        }
+        return isValid;
+    }
+
+    displayMessage(message, className) {
+        this.form.insertAdjacentHTML('beforebegin', 
         `
-            <div>
-                <h2>Form Submitted</h2>
+            <div class="form-message form-message--${className}">
+                <h2>${message}</h2>
             </div>
         `);
+
+        this.removeMessageAfterThreeSeconds(".form-message");
+    }
+
+    removeMessageAfterThreeSeconds(selector) {
+        setTimeout(() => {
+            document.querySelector(selector).remove();
+        }, 3000);
     }
 }
 
