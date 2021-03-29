e1296d680ffaf453f7c2e2d2d3326ee5970b1c18
Rich Ramos <richard.ramos3124@gmail.com>
2021-03-29 16:23:46 -0400

    completed gmail-api feature

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -4,13 +4,11 @@ import Modal from "./modules/Modal";
 import Filter from "./modules/Filter";
 import RevealOnScroll from "./modules/RevealOnScroll"
 import Form from "./modules/Form";
-// import Mail from "./modules/Mail";
 
 new RevealOnScroll(document.querySelectorAll(".service-item"), 75);
 var mobileMenu = new MobileMenu();
 new Modal();
 new Filter();
-// new Mail();
 new Form();
 
 
