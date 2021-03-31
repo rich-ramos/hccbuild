fa2fb34212923122757b7a6710e18cc4dbdcce2b
Rich Ramos <richard.ramos3124@gmail.com>
2021-03-31 14:01:02 -0400

    Created validate module to handle validation of input fields from a form

---
+++ src/assets/scripts/modules/validate/factory/ValidateFactory.js
@@ -0,0 +1,30 @@
+import NameValidate from '../NameValidate';
+import PhoneValidate from '../PhoneValidate';
+import EmailValidate from '../EmailValidate';
+import ServiceValidate from '../ServiceValidate';
+import ZipValidate from '../ZipValidate';
+import MessageValidate from '../MessageValidate';
+
+class ValidateFactory {
+    get validateTypes() {
+        return { 
+            NameValidate,
+            PhoneValidate,
+            EmailValidate, 
+            ServiceValidate, 
+            ZipValidate, 
+            MessageValidate 
+        };
+    }
+    static createValidate(type, target) {
+        let validateBase = this.prototype.validateTypes[type];
+
+        return (validateBase === undefined) 
+            ? null
+            : (validateBase.name === ServiceValidate.name) 
+            ? new validateBase.prototype.constructor(target) 
+            : new validateBase.prototype.constructor(target.value); 
+    }
+}
+
+export default ValidateFactory;
